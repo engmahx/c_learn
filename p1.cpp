11aa2533ae679fe9b690ddb24ccd7e02e670ae1d
engmahx <engmahx@users.noreply.github.com>
2021-09-22 23:00:02 +0200

    updated

--- p1.cpp
+++ p1.cpp
@@ -3,19 +3,26 @@
 #include<tuple>
 using namespace std;
 
-auto func = []()
+auto func()
 {
     int x= 9;
     string s = "some string too";
+
+    tuple<int,string> k(x,s);
+
+    int g = get<0>(k);
+    cout << "g :"<<  g << "\n";
+    auto [y,r] = k;
+    cout << "r :"<<  r << "\n";
     
-    return tuple<int,string>(x,s);
+    return k;
 };
 
 int main()
 {
     auto [x,s] = func();
-    std::cout << "&func: " << &func << " - func: " << func << std::endl;
+    //std::cout << "&func: " << &func << " - func: " << func << std::endl;
     std::cout << "VS code form windows, clang from wsl !!!!" << std::endl;
-    std::cout << "x: " << "s : " << s << std::endl;
+    std::cout << "x: " << x << " s : " << s << std::endl;
     return 0;
 }
